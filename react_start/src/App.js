0a381476e12abc4247d3cb0bf603e27f766e7832
Mahi <170040338@kluniversity.in>
2022-01-29 13:10:46 +0530

    Props

--- react_start/src/App.js
+++ react_start/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Raju">We are good</Greet>
+      <Greet name ="DJ">We are bad</Greet>
+      <Greet name="Kaju">All are good</Greet>
+      <Welcome name="DK"/>
     </div>
   );
 }
